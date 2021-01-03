ddc020e956ebe6664811f3227b13235d1433df2d
Veggie <cmderochie+github@gmail.com>
2021-01-03 00:19:01 -0700

    Made the extra cell match the border
    Saved the two colours to variables and swap based on startingTeam. Explicitly use startingTeamColor on the set of 9 cells and otherTeamColor on the set of 8.

--- codenames.js
+++ codenames.js
@@ -5,21 +5,22 @@ $( document ).ready(function() {
         var counter = 0;
         var previous_values = new Set();
         var startingTeam = Math.round(Math.random());
-
+        
+        var startingTeamColor = "#1e88e5";
+        var otherTeamColor = "#d32f2f";
         if (startingTeam == 1){
-            $("#grid").css("border-color","#d32f2f");
-        }else{
-            $("#grid").css("border-color","#1e88e5");
+            [startingTeamColor, otherTeamColor] = [otherTeamColor, startingTeamColor];
         }
+        $("#grid").css("border-color",startingTeamColor);
 
         while (counter < 18 ){
             var random = Math.floor(Math.random() * (25 - 1 + 1)) + 1;
             if (!previous_values.has(random)){
                 if (counter < 8){
-                    $(`#${random}`).css("background-color","#d32f2f");
+                    $(`#${random}`).css("background-color",otherTeamColor);
                 }
                 else if (counter > 8 && counter < 25){
-                    $(`#${random}`).css("background-color","#1e88e5");
+                    $(`#${random}`).css("background-color",startingTeamColor);
                 }
                 else {
                     $(`#${random}`).css("background-color","grey");
